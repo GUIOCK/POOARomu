ee8f7f2946afa0c4d7dfb7a9a509e3eaae23a3eb
KF <kevin.fagot@edu.itescia.fr>
2019-04-01 11:37:31 +0200

    Commit for Antonin full code + change ChessModel

--- Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/ChessModel.java
+++ Romuald78-chesspublic-575597261666/src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -5,10 +5,92 @@
  */
 package fr.rphstudio.chess.game;
 
+import fr.rphstudio.chess.interf.EmptyCellException;
+import fr.rphstudio.chess.interf.IChess;
+import fr.rphstudio.chess.interf.OutOfBoardException;
+import java.util.List;
+
 /**
  *
  * @author azz-kevinf
  */
-public class ChessModel {
+public class ChessModel implements IChess{
+    
+    private static ChessModel INSTANCE;
+    
+    private ChessModel(){
+        
+    }
+    
+    public static ChessModel getInstance(){
+        if(ChessModel.INSTANCE == null){
+            ChessModel.INSTANCE = new ChessModel();
+    
+        }
+        return ChessModel.INSTANCE;
+    }
+
+    
+    
+    
+    
+    @Override
+    public void reinit() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    @Override
+    public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public int getNbRemainingPieces(ChessColor color) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 0;
+    }
+
+    @Override
+    public List<ChessPosition> getPieceMoves(ChessPosition p) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public void movePiece(ChessPosition p0, ChessPosition p1) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ChessKingState getKingState(ChessColor color) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public List<ChessType> getRemovedPieces(ChessColor color) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    @Override
+    public boolean undoLastMove() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
+    }
+
+    @Override
+    public long getPlayerDuration(ChessColor color, boolean isPlaying) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 50010101;
+    }
     
 }
